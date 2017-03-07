f459e8847a2af6c1af3de4f2b11531c384546598
hzlifan <hzlifan@corp.netease.com>
2017-03-07 14:59:08 +0800

    add POST demo

--- src/main/java/com/lee/demo/eureka/controller/ConsumerController.java
+++ src/main/java/com/lee/demo/eureka/controller/ConsumerController.java
@@ -7,6 +7,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * 调用远程服务"compute-service"，利用Ribbon做负载均衡
  *
@@ -19,10 +22,10 @@ public class ConsumerController {
     private RestTemplate        restTemplate;
 
     // 远程服务名
-    private static final String SERVICE_NAME = "compute-service".toUpperCase();
+    private static final String SERVICE_NAME = "compute-service";
 
     /**
-     * 调用远程服务，传入URL和返回值的类型，返回内容
+     * 调用远程服务（GET），传入URL和返回值的类型，返回结果
      * 同时指定出错时的回调方法fallbackMethod
      *
      * @return
@@ -35,11 +38,27 @@ public class ConsumerController {
     }
 
     /**
+     * 调用远程服务（POST），传入URL、请求体和返回值的类型，返回结果
+     * 同时指定出错时的回调方法fallbackMethod
+     *
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "addServiceFallback")
+    @RequestMapping(value = "/ribbon/sub", method = RequestMethod.GET)
+    public String sub() {
+        Map<String, Object> params = new HashMap<String, Object>();  // POST请求体
+        params.put("a", 7);
+        params.put("b", 3);
+        return restTemplate.postForEntity("http://" + SERVICE_NAME + "/sub", params, String.class)
+            .getBody();
+    }
+
+    /**
      * 出错时的回调方法
      *
      * @return
      */
-    private String addServiceFallback(){
+    private String addServiceFallback() {
         return "This is a self-defined error message with Hystrix by Fernando";
     }
 
